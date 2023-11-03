122c44bfa9a5ef51ac38461244a2c784d286757e
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2023-11-03 13:50:47 -0700

    ♻️ Use function 'add_color()'

--- src/loader.py
+++ src/loader.py
@@ -3,6 +3,9 @@ import pandas as pd
 import json
 import plotly.express as px
 
+from mapita_streamlit import add_color
+
+
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
     traps = json.load(uploaded_file)
@@ -11,15 +14,10 @@ if uploaded_file is not None:
     lon = [feature["properties"]["lon"] for feature in traps["features"]]
     id = [feature["properties"]["ID"] for feature in traps["features"]]
     line = [feature["properties"]["line"] for feature in traps["features"]]
-    colour = [
-        "Activa" if feature["properties"]["is_active"] else "Inactiva"
-        for feature in traps["features"]
-    ]
     is_active = [feature["properties"]["is_active"] for feature in traps["features"]]
 
-    df = pd.DataFrame(
-        {"lat": lat, "lon": lon, "color": colour, "ID": id, "line": line, "is_active": is_active}
-    )
+    df = pd.DataFrame({"lat": lat, "lon": lon, "ID": id, "line": line, "is_active": is_active})
+    df = add_color(df)
 
     lines_df = pd.DataFrame({"line": ["Linea Cerco", "Linea 1"], "is_active": [False, False]})
 
