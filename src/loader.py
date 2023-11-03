3db9d27778c8453f5b410557bc9a56e28fee2a78
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2023-11-03 14:59:13 -0700

    🚧 Use function 'update_trap_line()'

--- src/loader.py
+++ src/loader.py
@@ -3,7 +3,7 @@ import pandas as pd
 import json
 import plotly.express as px
 
-from mapita_streamlit import add_color
+from mapita_streamlit import add_color, update_trap_lines
 
 
 uploaded_file = st.file_uploader("Choose a file")
@@ -18,13 +18,15 @@ if uploaded_file is not None:
 
     df = pd.DataFrame({"lat": lat, "lon": lon, "ID": id, "line": line, "is_active": is_active})
     df = add_color(df)
-
+    
     lines_df = pd.DataFrame({"line": ["Linea Cerco", "Linea 1"], "is_active": [False, False]})
 
     edited_df = st.data_editor(lines_df, hide_index=True)
 
+    map_df = update_trap_lines(df, edited_df)
+    
     fig = px.scatter_mapbox(
-        df,
+        map_df,
         lat="lat",
         lon="lon",
         hover_name="ID",
