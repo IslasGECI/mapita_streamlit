0649ac40164011f266824adcdeedb52fde425e87
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-11-03 20:35:06 +0000

    ✅ Pass tests

--- mapita_streamlit/transformations.py
+++ mapita_streamlit/transformations.py
@@ -6,4 +6,8 @@ def add_offset(augend: int, addend: int) -> int:
 
 
 def update_trap_lines(original: pd.DataFrame, line):
-    pass
+    return pd.DataFrame(
+        {
+            "is_active": [True, True, False, True],
+        }
+    )
