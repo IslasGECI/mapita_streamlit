92bdf8577b04c01b713746bf95ab8caac9ea1d1a
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-11-03 20:55:14 +0000

    🛑🧪 Fail tests

--- tests/test_transformations.py
+++ tests/test_transformations.py
@@ -1,8 +1,6 @@
 from mapita_streamlit import update_trap_lines
 import pandas as pd
 
-
-line = pd.DataFrame({"line": ["linea 1", "cerco"], "is_active": [True, False]})
 original = pd.DataFrame(
     {
         "line": ["linea 1", "linea 1", "cerco", "Linea 2"],
@@ -11,6 +9,8 @@ original = pd.DataFrame(
         "color": ["Inactiva", "Inactiva", "Inactiva", "Activa"],
     }
 )
+
+line = pd.DataFrame({"line": ["linea 1", "cerco"], "is_active": [True, False]})
 expected = pd.DataFrame(
     {
         "line": ["linea 1", "linea 1", "cerco", "Linea 2"],
@@ -20,8 +20,21 @@ expected = pd.DataFrame(
     }
 )
 
+line_2 = pd.DataFrame({"line": ["linea 1", "cerco"], "is_active": [False, False]})
+expected_2 = pd.DataFrame(
+    {
+        "line": ["linea 1", "linea 1", "cerco", "Linea 2"],
+        "is_active": [False, False, False, True],
+        "ID": [1, 2, 3, 4],
+        "color": ["Inactiva", "Inactiva", "Inactiva", "Activa"],
+    }
+)
+
 
 def test_update_trap_lines():
     obtained = update_trap_lines(original, line)
     assert (obtained.is_active == expected.is_active).all()
     assert (obtained.color == expected.color).all()
+
+    obtained = update_trap_lines(original, line_2)
+    assert (obtained.is_active == expected_2.is_active).all()
