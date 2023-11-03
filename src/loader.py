e43ae290c8b1a9cb7160a3ade5a9177b4494f27b
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2023-11-03 13:18:11 -0700

    🎨 Format with Black

--- src/loader.py
+++ src/loader.py
@@ -5,7 +5,6 @@ import plotly.express as px
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
-
     traps = json.load(uploaded_file)
 
     lat = [feature["properties"]["lat"] for feature in traps["features"]]
@@ -13,17 +12,19 @@ if uploaded_file is not None:
     id = [feature["properties"]["ID"] for feature in traps["features"]]
     line = [feature["properties"]["line"] for feature in traps["features"]]
     colour = [
-        "Activa" if feature["properties"]["is_active"] else "Inactiva" for feature in traps["features"]
+        "Activa" if feature["properties"]["is_active"] else "Inactiva"
+        for feature in traps["features"]
     ]
     is_active = [feature["properties"]["is_active"] for feature in traps["features"]]
 
+    df = pd.DataFrame(
+        {"lat": lat, "lon": lon, "color": colour, "ID": id, "line": line, "is_active": is_active}
+    )
 
-    df = pd.DataFrame({"lat": lat, "lon": lon, "color": colour, "ID": id, "line": line, "is_active": is_active})
-    
     lines_df = pd.DataFrame({"line": ["Linea Cerco", "Linea 1"], "is_active": [False, False]})
-    
+
     edited_df = st.data_editor(lines_df, hide_index=True)
-    
+
     fig = px.scatter_mapbox(
         df,
         lat="lat",
@@ -36,4 +37,3 @@ if uploaded_file is not None:
     )
     fig.update_layout(mapbox_style="open-street-map")
     st.plotly_chart(fig)
-
