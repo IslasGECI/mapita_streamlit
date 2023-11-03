33cc21d09bf1de4e67e7b950cad0d842a05b0276
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2023-11-03 13:19:03 -0700

    🚧✔️  Set up test

--- tests/test_transformations.py
+++ tests/test_transformations.py
@@ -1,4 +1,5 @@
 import mapita_streamlit as dt
+import pandas as pd
 
 
 def test_add_offset():
@@ -7,3 +8,22 @@ def test_add_offset():
     expected = augend + addend
     obtained = dt.add_offset(augend, addend)
     assert expected == obtained
+
+
+line = pd.DataFrame({"line": ["linea 1", "cerco"], "is_active": [True, False]})
+original = pd.DataFrame(
+    {
+        "line": ["linea 1", "linea 1", "cerco", "Linea 2"],
+        "is_active": [False, False, False, True],
+        "ID": [1, 2, 3, 4],
+        "color": ["Inactiva", "Inactiva", "Inactiva", "Activa"],
+    }
+)
+expected = pd.DataFrame(
+    {
+        "line": ["linea 1", "linea 1", "cerco", "Linea 2"],
+        "is_active": [True, True, False, True],
+        "ID": [1, 2, 3, 4],
+        "color": ["Activa", "Activa", "Inactiva", "Activa"],
+    }
+)
